1e6f087cea19762c70448003d3536b2d788c173b
Tin Nguyen <ngutt094@mymail.unisa.edu.au>
2021-06-01 20:59:52 +0930

    Added in selenium to read the dynamic website.

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("hello world")
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+url = "https://www.thelott.com/"
+
+driver = webdriver.Chrome('./chromedriver') 
+driver.get(url)
+
+time.sleep(5) 
+
+html = driver.page_source
+
+soup = BeautifulSoup(html, "html.parser")
+
+powerballSection = soup.find_all('li', class_='au-target powerball state-sa')
+

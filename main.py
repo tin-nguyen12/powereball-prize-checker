70e4d0445d853b6b1839c271c83a9afd7d2404a9
Tin Nguyen <ngutt094@mymail.unisa.edu.au>
2021-06-01 22:09:18 +0930

    Created the basic application so that it will email me when the powerball is over a certain amount

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import requests
+import smtplib
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+import re
+
+def getPrizeMoney(amount):
+    return int(re.findall("\d+", amount)[0])
 
 url = "https://www.thelott.com/"
 
@@ -16,4 +21,20 @@ html = driver.page_source
 soup = BeautifulSoup(html, "html.parser")
 
 powerballSection = soup.find_all('li', class_='au-target powerball state-sa')
+
+for section in powerballSection:
+    amount = getPrizeMoney(section.find('div',class_='au-target jackpot').getText())
+
+if amount > 40:
+    conn = smtplib.SMTP('smtp.gmail.com', 587)
+    conn.ehlo()
+    conn.starttls()
+    conn.login('tinothynguyen12@gmail.com', 'ysoomdieepmngezw')
+    conn.sendmail('tinothynguyen12@gmail.com', 'tinothynguyen@gmail.com', 'Subject: Powerball Alert!!\n\nA prize pool of %d million has been announced!' % amount)
+    conn.quit()
+
+else:
+    driver.close()
+    driver.quit()
+
 
